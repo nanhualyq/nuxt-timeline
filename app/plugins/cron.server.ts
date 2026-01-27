335a910334a33ffd33d0209cae0e05ccc42dca6f
lyq <nanhualyq@gmail.com>
2026-01-27 20:58:42 +0800

    fix: warng setInterval()

--- app/plugins/cron.server.ts
+++ app/plugins/cron.server.ts
@@ -1,15 +1,19 @@
-export default defineNuxtPlugin(() => {
-  setInterval(
-    () => {
-      try {
-        refreshAllSubscription();
-        console.log("定时任务每小时运行一次");
-      } catch (err) {
-        console.error("定时任务出错：", err);
-      }
-    },
-    1000 * 60 * 60,
-  );
+export default defineNuxtPlugin({
+  hooks: {
+    "app:mounted"() {
+      setInterval(
+        () => {
+          try {
+            refreshAllSubscription();
+            console.log("定时任务每小时运行一次");
+          } catch (err) {
+            console.error("定时任务出错：", err);
+          }
+        },
+        1000 * 60 * 60,
+      );
 
-  console.log("cron 定时任务已启动（服务端）");
+      console.log("cron 定时任务已启动（服务端）");
+    },
+  },
 });
