f05f62b541eb5106cdf4e6282318664c5b6f7139
lyq <nanhualyq@gmail.com>
2026-01-27 20:41:36 +0800

    fix: node-cron error on prod

--- app/plugins/cron.server.ts
+++ app/plugins/cron.server.ts
@@ -1,14 +1,15 @@
-import cron from "node-cron";
-
 export default defineNuxtPlugin(() => {
-  cron.schedule("0 * * * *", async () => {
-    try {
-      refreshAllSubscription();
-      console.log("定时任务每小时运行一次");
-    } catch (err) {
-      console.error("定时任务出错：", err);
-    }
-  });
+  setInterval(
+    () => {
+      try {
+        refreshAllSubscription();
+        console.log("定时任务每小时运行一次");
+      } catch (err) {
+        console.error("定时任务出错：", err);
+      }
+    },
+    1000 * 60 * 60,
+  );
 
   console.log("cron 定时任务已启动（服务端）");
 });
