ac6402296c283a4c23bf2e975dcf6df8be3bb96d
lyq <nanhualyq@gmail.com>
2026-02-07 13:28:32 +0800

    fix: enter lead other button triggered

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -33,10 +33,7 @@
           <span>
             {{ formatDistance(item.time, new Date()) }}
           </span>
-          <NuxtLink
-            @click.stop
-            :to="`/?subscription=${item.subscription_id}`"
-          >
+          <NuxtLink @click.stop :to="`/?subscription=${item.subscription_id}`">
             <img
               v-if="item.subscription.icon"
               :src="item.subscription.icon"
@@ -269,6 +266,9 @@ onPureKey("m", (e) => {
   markRead(activeIndex.value, false);
 });
 onPureKey("Enter", (e) => {
+  if (activeIndex.value >= 0) {
+    e.preventDefault();
+  }
   setTimeout(() => {
     openContentModal(activeIndex.value);
   }, 0);
