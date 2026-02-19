b1655b3db45ec7141fc3dcfdef05ee34cc61ead6
lyq <nanhualyq@gmail.com>
2026-02-19 20:58:31 +0800

    test: error show when test_code()

--- server/api/subscription/test_code.post.ts
+++ server/api/subscription/test_code.post.ts
@@ -15,7 +15,10 @@ export default defineEventHandler(async (event) => {
   } catch (error) {
     throw createError({
       statusCode: 500,
-      statusMessage: error + "",
+      statusMessage: "error",
+      data: {
+        detail: error + "",
+      },
     });
   }
 });
